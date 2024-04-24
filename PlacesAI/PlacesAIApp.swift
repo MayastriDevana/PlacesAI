e8a6596e99b93aff8be395114de8ea8a23c82390
Maya <Mayastridevana0205@gmail.com>
2024-04-24 10:20:42 +0700

    Initial Commit

---
+++ PlacesAI/PlacesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAIApp.swift
+//  PlacesAI
+//
+//  Created by User on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PlacesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
