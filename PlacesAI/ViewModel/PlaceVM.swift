789bb95f392d031a7054633a4765fe3c7b3156b6
Maya <Mayastridevana0205@gmail.com>
2024-04-26 14:22:43 +0700

    Adjust PlacesVM with the new RemoteConfigService

--- PlacesAI/ViewModel/PlaceVM.swift
+++ PlacesAI/ViewModel/PlaceVM.swift
@@ -9,15 +9,32 @@ import Foundation
 import GoogleGenerativeAI
 
 class PlaceVM: ObservableObject{
-    private var geminiModel: GenerativeModel
+    private var geminiModel: GenerativeModel?
     @Published var place: [Place] = []
     
     init() {
-        self.geminiModel = GenerativeModel(name: "gemini-pro", apiKey: Constants.apiKey)
+//        self.geminiModel = GenerativeModel(name: "gemini-pro", apiKey: Constants.apiKey)
+        configureGenerativeModel()
+    }
+    
+    func configureGenerativeModel() {
+        Task{
+            do{
+                let apiKey: String = try await RemoteConfigService.shared.fetchConfig(forKey: .apiKey)
+                self.geminiModel = GenerativeModel(name: "gemini-pro", apiKey: apiKey)
+            }catch{
+                print("Error configuring GenerativeModel: \(error)")
+                
+            }
+        }
     }
     
     func getPlaces() async{
         let prompt = Constants.prompt
+        guard let geminiModel = self.geminiModel else {
+            print("Generative Model is not configured")
+            return
+        }
         
         do{
             let response = try await geminiModel.generateContent(prompt)
